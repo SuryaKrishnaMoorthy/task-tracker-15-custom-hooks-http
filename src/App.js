3debe675826246663f0ea1838bc3ffa4f621cb90
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-10-23 11:15:32 -0400

    add tasks and task item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Tasks from './components/Tasks/Tasks';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://view-movie-list-14-httpreq-default-rtdb.firebaseio.com/tasks.json')
+      if(!response.ok) {
+        throw new Error('Request failed')
+      }
+  
+      const data = await response.json();
+      const loadedTasks = [];
+  
+      for(const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text })
+      }
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong')
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Tasks
+        items={tasks} 
+        error={error}
+        loading={isLoading}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
